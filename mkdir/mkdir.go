d25bd3818c2c0005702a57450f29bc7520f8bcea
def4root <def4root@proton.me>
2023-06-05 12:12:43 +0900

    mkdir command

--- mkdir/mkdir.go
+++ mkdir/mkdir.go
@@ -6,6 +6,11 @@ import (
 )
 
 func main() {
+	if len(os.Args) < 2 {
+		fmt.Println("Usage: ", os.Args[0], "dirname")
+		return
+	}
+
 	err := os.Mkdir(os.Args[1], 0755)
 	if err != nil {
 		fmt.Println("Directory create error:", err)
